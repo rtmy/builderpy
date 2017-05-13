f263956781b2106fc8e0cecdd6fffb6a5a752c3a
Artemy <randpaul@ya.ru>
2017-05-13 14:20:39 +0000

    minor changes

--- builder.py
+++ builder.py
@@ -14,25 +14,37 @@ class RequestHandler(http.server.CGIHTTPRequestHandler):
 
 		self.wfile.write(bytes('Please send POST', 'utf8'))
 	def do_POST(self):
+		# Send response status
 		self.send_response(200)		
 
 		self.send_header('Content-type','application/json')
 		self.end_headers()
 
-		action = self.headers["type-action"]
-		repository = self.headers["repository"]
+		data = self.rfile.read(65535).decode()
+		print(data)
+		
+		contents = json.loads(data)
+
+		action = contents["type-action"]
+		repository = contents["repository"]
+
+		if not action or not repository:
+			self.wfile.write(bytes('{"status":"error", "content":{"text":"post, please"}}', "utf8"))
+			return
 
 		print(repository)
 		pattern = re.compile("https:\/\/github\.com\/(?P<user>[A-z]+)\/(?P<repo>[A-z]+)")
 
 		match = pattern.match(repository)
 
+		if not match:
+			self.wfile.write(bytes('{"status":"error", "content":{"text":"repo not compatible"}}', "utf8"))
+			return
+
 		user = match.group('user')
 		repo = match.group('repo')
 
-		if not match:
-			output = {'status':'error', 'content':{'text':'repo url is not compatible'}}
-		elif action == "build":
+		if action == "build":
 			url = 'https://github.com/' + user + '/' + repo 
 			repodir = gitget(url)
 			output = build(repodir)
@@ -54,28 +66,17 @@ def run(server_class=http.server.HTTPServer, handler_class=RequestHandler):
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
-def gitget(URL):
-	'''Git fetch and pull.
-
-       This function purpose is to download \
-       repository content using provided URL.
-       Returns path where is repository pulled, \
-       string.'''
-	reponame = os.path.basename(URL)
+def gitget(url):
+	reponame = os.path.basename(url)
 	repodir = os.path.join(os.getcwd(), reponame + time.strftime("%Y%m%d%H%M%S"))
 	repo = git.Repo.init(repodir)
-	origin = repo.create_remote('origin', URL)
+	origin = repo.create_remote('origin', url)
 	origin.fetch()
 	repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
 	origin.pull()
 	return repodir
 
 def build(repodir):
-	'''Repository basic check and build routines.
-
-       This function checks specifications file
-       options and tries to build each file.
-       Returns status and additional stuff.'''
 	cflags = ['--std=c89', '-Wall', '-Werror']
 
 	for dir in next(os.walk(repodir))[1]:
@@ -96,6 +97,7 @@ def build(repodir):
 	else:
 		return {'status':'error', 'content':{'text':'language is not supported'}}
 
+	## проверять флаги
 	flags = data["flags"]
 	for flag in flags:
 		if flag not in cflags:
@@ -124,15 +126,6 @@ def build(repodir):
 	return {'status':'ok', 'content':{'text':'...'}}
 
 def retrieve(reponame, date='latest'):
-	'''Returns log.
-
-       This function checks if there is \
-       a log file on disk and returns its content \
-       as JSON.'''
-
-	if not os.path.exists('logs'):
-		return {'status':'error', 'content':{'text':'logfile not found'}}
-
 	for filename in next(os.walk('logs'))[2]:
 		print(filename)
 		if reponame in filename:
@@ -143,11 +136,9 @@ def retrieve(reponame, date='latest'):
 	except NameError: ## ??!?
 		return {'status':'error', 'content':{'text':'logfile not found'}}
 
-	logfile = open(os.path.join('logs', logfile))
+	logfile = open(os.path.join('logs', 	logfile))
 	output =  logfile.read()
 	logfile.close()
 	return {'status':'ok', 'content':{'filename':filename, 'text':output}}
-
-	pass
 
 run()
